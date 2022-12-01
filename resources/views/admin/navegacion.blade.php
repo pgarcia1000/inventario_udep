44fb94d52bcd0bc5d0f06bdf9a9b22e478f4ba2d
Pedro G <pgarcia1000@gmail.com>
2022-11-30 22:29:46 -0600

    CAmbiar titulo  de inventario a inventario udep

--- resources/views/admin/navegacion.blade.php
+++ resources/views/admin/navegacion.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#243f59!important;">
-  <a class="navbar-brand" href="#">Inventario</a>
+  <a class="navbar-brand" href="#">Inventario UDEP</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
